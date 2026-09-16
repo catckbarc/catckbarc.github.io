e70338ef40d5e0494331bc320c790b0001f0ee61
catckbarc <touhoupensive@gmail.com>
2026-09-16 14:05:00 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","640ec4ccab5e9677bd8602a7df531fec"]];
+var precacheConfig = [["index.html","be057337c08ce7274a1019ca0351e984"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
